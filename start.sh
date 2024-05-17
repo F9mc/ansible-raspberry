386d7a5c18e3a2962cf65110f24361414b432741
pcabon <pcabon@almond.eu>
2024-05-17 10:52:49 +0200

    fix

--- start.sh
+++ start.sh
@@ -9,14 +9,12 @@ export USR="debian"
 echo "> Adding $USR to sudoers"
 /usr/sbin/usermod -aG sudo $USR
 
-sudo -u $USR bash
-
 # Clone repo
 echo "> Clonning dotfiles repo"
 cd /home/$USR
-git clone https://github.com/F9mc/dotfiles.git
+sudo -u $USR "git clone https://github.com/F9mc/dotfiles.git"
 cd dotfiles
 
 # Run playbook
 echo "> Starging playbook"
-ansible-playbook -k bootstrap.yml
+sudo -u $USR "ansible-playbook -k bootstrap.yml"
