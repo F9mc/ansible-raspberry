baf820cd3dcd869f85ca93f48617fdfe89f58113
pcabon <pcabon@almond.eu>
2024-05-17 10:36:31 +0200

    fix start

--- start.sh
+++ start.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # Uses root user
-su
+echo "> Connect as root"
+su -l root
 
 # Install git, ansible
+echo "> Installing default packages"
 apt install git ansible -y
 
 # Set "debian" as a Sudoers
 export USERNAME="debian"
-/use/sbin/usermod -aG sudo $USERNAME
+echo "> Adding $USERNAME to sudoers"
+/usr/sbin/usermod -aG sudo $USERNAME
 
 su -l $USERNAME
 
 # Clone repo
+echo "> Clonning dotfiles repo"
 cd /home/$USERNAME
 git clone https://github.com/F9mc/dotfiles.git
 cd dotfiles
 
 # Run playbook
+echo "> Starging playbook"
 ansible-playbook -k bootstrap.yml
