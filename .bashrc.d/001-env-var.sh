ea520a72a4d0962dea6540a5550d77913c735d03
f9mc <contact@pcabon.com>
2024-05-20 12:26:08 +0200

    add go path

--- .bashrc.d/001-env-var.sh
+++ .bashrc.d/001-env-var.sh
@@ -2,3 +2,4 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/mnt/c/Users/pcabon/AppData/Local/Programs/Microsoft VS Code/bin:$PATH"
 export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
