07f32d587d91f837ba71fac436b22252d26d8a07
f9mc <contact@pcabon.com>
2024-05-26 12:10:42 +0200

    add font

--- .zshrc.d/004-aliases.sh
+++ .zshrc.d/004-aliases.sh
@@ -1,5 +1,5 @@
 alias grep="grep --color"
-alias ls="exa"
+alias ls="exa --icons --group-directories-first --color=always --git"
 alias cat="batcat"
-alias cd="z"
+alias cd="zoxide"
 alias dotfiles="ansible-playbook -K ~/dotfiles/bootstrap.yml && bash"
